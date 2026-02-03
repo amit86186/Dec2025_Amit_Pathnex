eedb3a1a8271826c26ac4be6cb0f5d35acbbcffc
Amit <amit86186@gmail.com>
2026-02-04 01:27:33 +0530

    added bash script in Coding Practise/project1

--- Coding Practise/project1/code.sh
+++ Coding Practise/project1/code.sh
@@ -1,11 +1,10 @@
-
 provider "aws" {
-  region = "ap-south-1
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0f5ee92e2d63afc18"
- instance_type = "t2.micro"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "terraform-ec2"
